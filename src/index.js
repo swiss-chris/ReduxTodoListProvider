7de6ee6e63880dbb5a56fc67515120de19b335f5
swiss-chris <chris.dickinson@web.de>
2019-06-21 17:19:27 +0100

    added Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import reducer from "./reducer";
 import App from "./App";
 
@@ -10,6 +12,11 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDom.render(<App store={store} />, document.getElementById("app"));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
 
 export default { reducer };
